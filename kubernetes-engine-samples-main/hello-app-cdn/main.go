04e8587975f7d2cc0e339bf9ad87936c0cdfa5bd
Prabu-12 <97876296+Prabu-12@users.noreply.github.com>
2022-07-05 16:07:48 +0530

    Sample-Build

--- kubernetes-engine-samples-main/hello-app-cdn/main.go
+++ kubernetes-engine-samples-main/hello-app-cdn/main.go
@@ -32,7 +32,7 @@ func main() {
 
 	// register hello function to handle all requests
 	server := http.NewServeMux()
-	server.HandleFunc("/", hello)
+	server.HandleFunc("/", Prabu)
 
 	// start the web server on port and accept requests
 	log.Printf("Server listening on port %s", port)
